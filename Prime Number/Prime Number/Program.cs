bc91f7ebdc6f3155dab942c2fb841283480121e9
Jsowmya08 <sowmya.jangam0807@gmail.com>
2021-10-11 22:54:57 -0700

    assignment

---
+++ Prime Number/Prime Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a, m = 0, PrimeIndicator = 0;
+            Console.Write("Please enter the number to check for Primality: ");
+            n = int.Parse(Console.ReadLine());
+            m = n / 2;
+            for (a = 2; a <= m; a++)
+            {
+                if (n % a == 0)
+                {
+                    Console.Write("The entered number is not  a Prime number .");
+                    PrimeIndicator = 1;
+                    break;
+                }
+            }
+            if (PrimeIndicator == 0)
+                Console.Write("The entered number is a Prime number . ");
+        }
+    }
+}
---
+++ stopwatch/stopwatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startTimer = 0;
+            int stopTimer = 0;
+            int elapsed = 0;
+
+            Random rnd = new Random();
+
+            Console.WriteLine("Stop Time is: " + stopTimer);
+
+            Random rnd1 = new Random();
+
+            Console.WriteLine("Start Time is : " + startTimer);
+
+            Random rnd2 = new Random();
+
+            Console.WriteLine("elapsed is : " + elaspsed);
+
+        }
+    }
+}
